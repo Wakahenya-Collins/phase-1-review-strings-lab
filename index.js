0108a1ec0571b7363ccd61784a46a4afe940b260
Collins Wakahenya <wakahenyacollins@gmail.com>
2023-06-13 20:11:31 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code in this file!
+const currentUser = 'Sandy Beaches';
+
+ const welcomeMessage = 'Welcome to Flatbook, + ${Sandy Beaches}!';
+
+ const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+ const shortGreeting = 'Welcome, ${S.slice(0, 1)}!';
+
+
+
+ console.log (welcomeMessage)
+
+ 
